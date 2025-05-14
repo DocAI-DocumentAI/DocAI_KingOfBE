b945b772e09f7a491e3dd38762ca3ef631e1a2a9
Magic Flexing <134113309+Nguyen-Huy-Phuc@users.noreply.github.com>
2025-05-14 13:47:57 +0700

    Update: create appshost

---
+++ DocAI.AppHost/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Hosting;
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+// Thêm các API project với proxy để tự động chọn port
+var aiAPI = builder.AddProject<Projects.AI_API>("apiservice-ai")
+    .WithHttpEndpoint(name: "ai-endpoint");
+var docAPI = builder.AddProject<Projects.Document_API>("apiservice-doc")
+    .WithHttpEndpoint(name: "doc-endpoint");
+var authAPI = builder.AddProject<Projects.Auth_API>("apiservice-auth")
+    .WithHttpEndpoint(name: "auth-endpoint");
+var notiAPI = builder.AddProject<Projects.Notification_API>("apiservice-noti")
+    .WithHttpEndpoint(name: "noti-endpoint");
+
+var apigatewayAPI = builder.AddProject<Projects.ApiGateway>("apiservice-apigateway")
+    .WithHttpEndpoint(name: "apigateway-endpoint")
+    .WithReference(aiAPI)
+    .WithReference(docAPI)
+    .WithReference(authAPI)
+    .WithReference(notiAPI);
+
+// Build và chạy ứng dụng
+builder.Build().Run();
