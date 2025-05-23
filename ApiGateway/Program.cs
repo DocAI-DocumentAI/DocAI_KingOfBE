8e91c9cfe9bb81d48275cf6e65890d4644e2c620
Magic Flexing <134113309+Nguyen-Huy-Phuc@users.noreply.github.com>
2025-05-23 20:26:08 +0700

    deploy 5000

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using NSwag;
+using NSwag.Generation.Processors.Security;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,26 +10,45 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+// Register the NSwag services
+builder.Services.AddOpenApiDocument(options =>
+{
+    options.Title = "DocAI Auth API";
+    options.Version = "v1";
+
+    options.AddSecurity("Bearer", new OpenApiSecurityScheme
+    {
+        Type = OpenApiSecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        Name = "Authorization",
+        In = OpenApiSecurityApiKeyLocation.Header,
+    });
+
+    options.OperationProcessors.Add(new AspNetCoreOperationSecurityScopeProcessor("Bearer"));
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+// if (app.Environment.IsDevelopment())
+// {
     app.MapOpenApi();
     
+    app.UseOpenApi();
     app.UseSwaggerUI(options =>
     {
-        options.SwaggerEndpoint("/openapi/v1.json", "Api Gateway V1");
+        options.RoutePrefix = "swagger"; 
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1"); 
     });
 
-    app.UseReDoc(options =>
-    {
-        options.SpecUrl("/openapi/v1.json");
-    });
-
-    app.MapScalarApiReference();
-}
+//     app.UseReDoc(options =>
+//     {
+//         options.SpecUrl("/openapi/v1.json");
+//     });
+//
+//     app.MapScalarApiReference();
+// }
 
 app.UseHttpsRedirection();
 
