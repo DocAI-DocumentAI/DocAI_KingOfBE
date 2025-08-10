499078e9984c47e75fe0ea568770610edd148b9e
Zafu <150423390+giaphu290@users.noreply.github.com>
2025-08-10 14:17:42 +0700

    Update

---
+++ Services/Notification/Notification.Domain/Migrations/20250810071428_INit1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Notification.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class INit1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "DocumentId",
+                table: "NotificationLogs",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "DocumentId",
+                table: "NotificationLogs",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
