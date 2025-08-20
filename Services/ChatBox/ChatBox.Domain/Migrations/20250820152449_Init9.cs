2c92d179e76d6873ee816f8f80f7572af5d3fc79
Zafu <150423390+giaphu290@users.noreply.github.com>
2025-08-21 03:41:31 +0700

    Reapply "update chat"
    This reverts commit 580d0b02cb4fba2236dfc41464bc76acaebaedc1.

---
+++ Services/ChatBox/ChatBox.Domain/Migrations/20250820152449_Init9.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChatBox.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init9 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ApplyToNewChats",
+                table: "UserPreferences");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ApplyToNewChats",
+                table: "UserPreferences",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
